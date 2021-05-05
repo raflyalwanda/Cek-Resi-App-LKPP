51da1cc2fdb4810b409afabba9430b164806b35c
Rafly Alwanda <54300675+raflyalwanda@users.noreply.github.com>
2021-05-05 15:41:23 +0700

    First Commit

---
+++ views/site/index.php
@@ -0,0 +1,102 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Cek Paket';
+?>
+<div class="site-index">
+
+    <div class="body-content">
+
+                <div class="row">
+                        <div class="col-md-12">
+                            <h2><b>CEK PAKET</b></h2>  
+                        </div>
+                        <div class="col-md-6" style="">
+                            <input type="text" placeholder="Input resi anda ..." size="80"></input>
+                        </div>
+                        <div class="col-md-6">
+                            <button> Cari </button>
+                        </div>
+                        <div class="col-md-12">
+                        <br>
+                            <input type="radio" id="" name="" value="">
+                            <label for="JNE"> JNE </label>
+                            <input type="radio" id="" name="" value="">
+                            <label for="TIKI"> TIKI </label>
+                            <input type="radio" id="" name="" value="">
+                            <label for="POS"> POS </label>
+                            <input type="radio" id="" name="" value="">
+                            <label for="Sicepat"> Sicepat </label>
+                            <input type="radio" id="" name="" value="">
+                            <label for="Anteraja"> Anteraja </label>
+                            <input type="radio" id="" name="" value="">
+                            <label for="Ninja"> Ninja </label>
+                            <input type="radio" id="" name="" value="">
+                            <label for="JNT"> JNT </label>
+                        </div>
+                </div>
+            </div>
+
+        <br>
+        <div class="row">
+            <div class="col-md-12">
+                <table style="width:100%; border:1px solid black;">
+                    <thead>
+                        <tr>
+                            <td>
+                                Nomor Resi
+                            </td>
+                            <td>
+                                Tanggal Pengiriman
+                            </td>
+                            <td>
+                                Pengirim
+                            </td>
+                            <td>
+                                Penerima
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+
+                            </td>
+                        <tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+                        <div class="col-md-12">
+                            <h4><b>Detail Pengiriman</b></h4>  
+                        </div>
+                        <div class="col-md-12">
+                        <table style="width:50%; border:1px solid black;">
+                    <thead>
+                        <tr>
+                            <td>
+                                Tanggal Waktu
+                            </td>
+                            <td>
+                                Deskripsi
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+
+                            </td>
+                        <tr>
+                    </tbody>
+                </table>
+                        </div>
+                      
+            </div>
+
+</div>
